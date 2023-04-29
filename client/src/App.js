aaccb25febb89ec422cf66330f8e7059c2658775
unknown <zhou7cathy@gmail.com>
2023-04-29 22:01:57 +0800

    add basic file structure and install package.json.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+        <Button type="primary">Primary Button</Button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
