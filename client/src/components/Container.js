36dc2b76577578b4250e1b5dd088917d4cb01a5d
unknown <zhou7cathy@gmail.com>
2023-05-01 22:09:48 +0800

    add login and signup button.

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Space } from 'antd';
 import Navigation from './Navigation'
 import Footer from './Footer'
 import PetResQ from '../assets/pet-resq.png'
@@ -8,9 +9,12 @@ import Slogan from '../assets/slogan.png'
 export default function Container() {
   return (
     <div>
-      {/* <h1>Pet ResQ</h1> */}
       <img src={PetResQ} alt='pet resQ logo' />
       <img src={Slogan} alt='pet resQ slogan' />
+      <Space wrap>
+        <Button className='login-btn'>Login</Button>
+        <Button className='signup-btn'>Sign Up</Button>
+      </Space>
       <Navigation className="navigation"/>
       <Footer />
     </div>
