4e6ca69497e22c76e377575baf84807f3fc259d0
unknown <zhou7cathy@gmail.com>
2023-05-08 21:45:15 +0800

    styled lostPet and foundPet page.

--- client/src/pages/Found.js
+++ client/src/pages/Found.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_FOUND_ANIMALS } from '../utils/queries';
+import { Image } from 'antd';
 
 export default function Found() {
   const { loading, data } = useQuery(QUERY_FOUND_ANIMALS);
@@ -15,19 +16,35 @@ export default function Found() {
           ) : (
             <div>
               {foundAnimals && foundAnimals.map((animal) => (
-              <div key={animal._id}>
-                <p>
-                  {animal.status} 
-                </p>
-                <p>
-                  {animal.name} 
-                </p>
-                <p>
-                  {animal.description} 
-                </p>
-                <p>
-                  {animal.animalType.name} 
-                </p>
+              <div key={animal._id} className='post-container'>
+                <div>
+                  {animal.image.map((image) => (
+                  <Image
+                    className='post-img'
+                    key={image}
+                    width={300}
+                    height={200}
+                    src={image}
+                  />
+                  ))}
+                </div>
+                <div className='post-detail'>
+                  <p>
+                    Post Since: {animal.postDate} 
+                  </p>
+                  <p>
+                    {animal.status} 
+                  </p>
+                  <p>
+                    {animal.animalType.name} 
+                  </p>
+                  <p>
+                    {animal.name} 
+                  </p>
+                  <p>
+                    {animal.description} 
+                  </p>
+                </div>
               </div>
             ))}
             </div>
--- client/src/pages/Lost.js
+++ client/src/pages/Lost.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_LOST_ANIMALS } from '../utils/queries';
-import {
-  Image,
-} from 'antd';
+import { Image } from 'antd';
 
 export default function Lost() {
   const { loading, data } = useQuery(QUERY_LOST_ANIMALS);
@@ -18,26 +16,35 @@ export default function Lost() {
           ) : (
             <div>
               {lostAnimals && lostAnimals.map((animal) => (
-              <div key={animal._id}>
-                <p>
-                  {animal.status} 
-                </p>
-                <p>
-                  {animal.name} 
-                </p>
-                <p>
-                  {animal.description} 
-                </p>
-                <p>
-                  {animal.animalType.name} 
-                </p>
+              <div key={animal._id} className='post-container'>
+                <div>
                 {animal.image.map((image) => (
-                <Image
-                  key={image}
-                  width={200}
-                  src={image}
-                />
-                ))}
+                  <Image
+                    className='post-img'
+                    key={image}
+                    width={300}
+                    height={200}
+                    src={image}
+                  />
+                  ))}
+                </div>
+                <div className='post-detail'>
+                  <p>
+                    Post Since: {animal.postDate} 
+                  </p>
+                  <p>
+                    Pet Status: {animal.status} 
+                  </p>
+                  <p>
+                    Aniaml Type: {animal.animalType.name} 
+                  </p>
+                  <p>
+                    Name: {animal.name} 
+                  </p>
+                  <p>
+                    Description: {animal.description} 
+                  </p>
+                </div>
               </div>
             ))}
             </div>
