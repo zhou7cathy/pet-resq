140f26a7760a5cf9b269ec89c62b4734cad5762b
unknown <zhou7cathy@gmail.com>
2023-05-22 19:42:58 +0800

    deploy

--- server/server.js
+++ server/server.js
@@ -23,9 +23,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
